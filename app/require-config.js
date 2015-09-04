85db28f99da4b446c261e7cc2e3de550e87ac209
Ryan Tanay <ryantanay@newevolution.org>
2015-09-04 08:58:58 -0500

    Forgot phaser in the path herp.

--- app/require-config.js
+++ app/require-config.js
@@ -5,10 +5,10 @@ require.config({
     bootstrap: "../lib/bower_components/bootstrap/dist/js/bootstrap.min",
     angularRoute: "../lib/bower_components/angular-route/angular-route",
     babel: "../lib/bower_components/requirejs-babel/babel-5.8.22.min",
+    phaser: "../lib/bower_components/phaser/build/phaser.min",
     jquery: "../lib/bower_components/jquery/dist/jquery.min",
     angular: "../lib/bower_components/angular/angular.min",
     firebase: "../lib/bower_components/firebase/firebase",
-    phaser: "../lib/bower_components/build/phaser.min",
     es6: "../lib/bower_components/requirejs-babel/es6"
   },
   shim: {
